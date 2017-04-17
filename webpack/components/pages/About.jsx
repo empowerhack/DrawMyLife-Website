0ac751be745cbd29a82be2d9e3ab309a1f3a643d
EChesters <elizabeth.chesters@gmail.com>
2017-04-17 13:48:18 +0100

    Fixed unfound main css - Added image to about

--- webpack/components/pages/About.jsx
+++ webpack/components/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
+import { PageHeader, Grid, Row, Col, Image } from 'react-bootstrap';
 
 import { about } from '../../copy/about.js';
 
@@ -7,6 +7,7 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="container">
+        <Image src="http://lorempixel.com/output/cats-q-c-1920-1200-8.jpg" className="cover-image"/>
         <Grid>
           <Row>
             <Col xs={12} sm={7} md={6} mdOffset={2} smOffset={1}>
