9398e3179f765e74769605c7b6a2d4378ba5328a
EChesters <elizabeth.chesters@gmail.com>
2017-04-17 13:04:32 +0100

    Issue #18 - Added dummy text for About page - Needs actual text

--- webpack/components/pages/About.jsx
+++ webpack/components/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
 
 import { about } from '../../copy/about.js';
 
@@ -7,8 +7,17 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="container">
-        <PageHeader>{ about.title }</PageHeader>
-        <p>{ about.lorem }</p>
+        <Grid>
+          <Row>
+            <Col xs={12} sm={7} md={6} mdOffset={2} smOffset={1}>
+              <PageHeader>{ about.title }</PageHeader>
+              <p>{ about.lorem }</p>
+
+              <h2>{ about.subtitle }</h2>
+              <p>{ about.lorem }</p>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
